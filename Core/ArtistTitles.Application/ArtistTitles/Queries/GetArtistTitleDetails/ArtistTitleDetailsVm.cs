8a7c603143a473ba7618f19ad6dc9b8fac79132a
Efim Shumkov <47829068+E1Georg@users.noreply.github.com>
2023-02-17 17:06:29 +0300

    Add files via upload

---
+++ Core/ArtistTitles.Application/ArtistTitles/Queries/GetArtistTitleDetails/ArtistTitleDetailsVm.cs
@@ -0,0 +1,56 @@
+﻿using ArtistTitles.Application.Common.Mappings;
+using ArtistTitles.Domain;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtistTitles.Application.ArtistTitles.Queries.GetArtistTitleDetails
+{
+    public class ArtistTitleDetailsVm : IMapWith<ArtistTitle>
+    {
+        public Guid Id { get; set; }
+        public string url { get; set; }
+        public double price { get; set; }
+        public string currencyId { get; set; }
+        public int categoryId { get; set; }
+        public string picture { get; set; }
+        public bool delivery { get; set; }
+        public string artist { get; set; }
+        public string title { get; set; }
+        public int year { get; set; }
+        public string media { get; set; }
+        public string description { get; set; }        
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<ArtistTitle, ArtistTitleDetailsVm>()
+            .ForMember(artistTitleVm => artistTitleVm.Id,
+            opt => opt.MapFrom(artistTitle => artistTitle.Id))
+            .ForMember(artistTitleVm => artistTitleVm.price,
+            opt => opt.MapFrom(artistTitle => artistTitle.price))
+            .ForMember(artistTitleVm => artistTitleVm.currencyId,
+            opt => opt.MapFrom(artistTitle => artistTitle.currencyId))
+            .ForMember(artistTitleVm => artistTitleVm.categoryId,
+            opt => opt.MapFrom(artistTitle => artistTitle.categoryId))
+            .ForMember(artistTitleVm => artistTitleVm.picture,
+            opt => opt.MapFrom(artistTitle => artistTitle.picture))
+            .ForMember(artistTitleVm => artistTitleVm.delivery,
+            opt => opt.MapFrom(artistTitle => artistTitle.delivery))
+            .ForMember(artistTitleVm => artistTitleVm.artist,
+            opt => opt.MapFrom(artistTitle => artistTitle.artist))
+            .ForMember(artistTitleVm => artistTitleVm.title,
+            opt => opt.MapFrom(artistTitle => artistTitle.title))
+            .ForMember(artistTitleVm => artistTitleVm.year,
+            opt => opt.MapFrom(artistTitle => artistTitle.year))
+            .ForMember(artistTitleVm => artistTitleVm.media,
+            opt => opt.MapFrom(artistTitle => artistTitle.media))
+            .ForMember(artistTitleVm => artistTitleVm.description,
+            opt => opt.MapFrom(artistTitle => artistTitle.description))
+            .ForMember(artistTitleVm => artistTitleVm.url,
+            opt => opt.MapFrom(artistTitle => artistTitle.url));
+        }
+    }
+}
---
+++ Core/ArtistTitles.Application/ArtistTitles/Queries/GetArtistTitleDetails/GetArtistTitleDetailsQueryValidator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using FluentValidation;
+
+namespace ArtistTitles.Application.ArtistTitles.Queries.GetArtistTitleDetails
+{
+    public class GetArtistTitleDetailsQueryValidator : AbstractValidator<GetArtistTitleDetailsQuery>
+    {
+        public GetArtistTitleDetailsQueryValidator() 
+        {
+            //RuleFor(artistTitle => artistTitle.Id).NotEqual(Guid.Empty);
+        }
+    }
+}
