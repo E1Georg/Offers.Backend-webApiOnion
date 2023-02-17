39977310db15a6585eb56c561d9d8501cb8486f6
Efim Shumkov <47829068+E1Georg@users.noreply.github.com>
2023-02-17 17:02:25 +0300

    Add files via upload

---
+++ Core/ArtistTitles.Application/Interfaces/IArtistTitlesDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ArtistTitles.Domain;
+
+namespace ArtistTitles.Application.Interfaces
+{
+    public interface IArtistTitlesDbContext
+    {
+        DbSet<ArtistTitle> ArtistTitles { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
