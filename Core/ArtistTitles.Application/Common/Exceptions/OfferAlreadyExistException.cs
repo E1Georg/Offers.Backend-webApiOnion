99e2212a91281ba632b5d0946e7fb3a296f105d2
Efim Shumkov <47829068+E1Georg@users.noreply.github.com>
2023-02-17 17:04:43 +0300

    Add files via upload

---
+++ Core/ArtistTitles.Application/Common/Exceptions/OfferAlreadyExistException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtistTitles.Application.Common.Exceptions
+{
+    public class OfferAlreadyExistException : Exception
+    {
+        public OfferAlreadyExistException(string name, object key) : base($"Entity \"{name}\" ({key}) already exist in database.") { }
+    }
+}
