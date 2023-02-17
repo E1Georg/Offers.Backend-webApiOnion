23b98758c2ba0c40b76466b981de7b749c42c339
Efim Shumkov <47829068+E1Georg@users.noreply.github.com>
2023-02-17 17:13:26 +0300

    Add files via upload

---
+++ Infrastructure/ArtistTitles.Persistence/ArtistTitlesDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ArtistTitles.Application.Interfaces;
+using ArtistTitles.Domain;
+using ArtistTitles.Persistence.EntityTypeConfigurations;
+
+namespace ArtistTitles.Persistence
+{
+    public class ArtistTitlesDbContext : DbContext, IArtistTitlesDbContext
+    {
+        public DbSet<ArtistTitle> ArtistTitles { get; set; }
+        public ArtistTitlesDbContext(DbContextOptions<ArtistTitlesDbContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new ArtistTitleConfiguration());
+            base.OnModelCreating(builder);
+        }
+    }
+}
